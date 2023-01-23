df16c58f003d7885940d8f3279241450ec46917b
jezelldomer <jzlldomer@gmail.com>
2023-01-22 22:25:39 -0800

    Adding set of arrays and colors to the text

--- selection-sort.py
+++ selection-sort.py
@@ -13,4 +13,10 @@ def selectionSort(array, size):
                          
         (array[step], array[minpos]) = (array[minpos], array[step])
 
-
+# arrays in the spreadsheet 
+data = [ 2, 27, 40, 7, 13, 66, 1, 31, 41, 24 ]
+size = len(data)
+selectionSort(data, size)
+print ("\033[1;33;40m╭── ⋅ ⋅ ── ✩ ── ⋅ ⋅─── ⋅ ⋅ ── ✩ ── ⋅ ⋅─── ⋅ ⋅ ── ✩ ── ⋅ ⋅──╮\033[1;33;40m\n")
+print('   \033[1;37;40mSorted Array in Ascending Order using Selection Sort:\033[1;37;40m\n')
+print("         \033[0;37;44m",data,"\033[0;37;40m\n")
