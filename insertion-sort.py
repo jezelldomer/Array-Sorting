708056fa3d9121cbb6eeb83f345d99313d215e41
jezelldomer <jzlldomer@gmail.com>
2023-01-22 23:16:32 -0800

    Adding data and fixing some errors plus adding colors

--- insertion-sort.py
+++ insertion-sort.py
@@ -3,12 +3,19 @@
 def insertionSort(array):
 
     for step in range(1, len(array)):
-        sort = array[step]
-        j = sort - 1
+        key = array[step]
+        j = step - 1
         
         # using > to sort in ascending order
-        while j >= 0 and sort < array[j]:
+        while j >= 0 and key < array[j]:
             array[j + 1] = array[j]
             j = j - 1
         
-        array[j + 1] = sort
+        array[j + 1] = key
+
+
+data = [ 2, 27, 40, 7, 13, 66, 1, 31, 41, 24 ]
+insertionSort(data)
+print ("\033[0;35;40m-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈-ˋˏ✄┈┈┈┈ˋˏ✄┈┈┈┈\n")
+print('    \033[1;37;40mSorted Array in Ascending Order using Insertion Sort:\033[1;37;40m\n')
+print("          \033[0;37;42m",data,"\033[0;37;40m\n")
