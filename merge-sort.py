c6aea12a975ef0a80d3f897cfe59e498bda18f24
jezelldomer <jzlldomer@gmail.com>
2023-01-23 00:02:54 -0800

    Function for merge sort

---
+++ merge-sort.py
@@ -0,0 +1,33 @@
+# Merge Sort 
+
+
+def mergeSort(array):
+    if len(array) > 1:
+
+        d = len(array)//2
+        L = array[:d]
+        M = array[d:]
+
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
